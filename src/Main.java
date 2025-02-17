00d5ac49d8bcc79060df39c9d5962fbe4a840ec5
HAGENSV22 <hagensv22@gcc.edu>
2025-02-17 15:08:33 -0500

    Create main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        /* Java 17 w/ Gradle
+         * Make intelliJ project
+         * Initialize version control (top bar)
+         * Commit ONLY the java file
+         * Menu > Git > Github
+         */
     }
 }
